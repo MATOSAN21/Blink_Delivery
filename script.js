4ceddd1cc7e244dc92cc619923b0390cd50e401b
Mateus Santos <mateuss.oliveira.212@gmail.com>
2026-07-14 17:40:09 +0100

    Primeira versão projeto Blink

---
+++ script.js
@@ -0,0 +1,14 @@
+document.getElementById("produtos").innerHTML = `
+<div style="
+background:#111;
+padding:30px;
+border-radius:15px;
+text-align:center;
+">
+
+<h3>Em breve</h3>
+
+<p>O catálogo estará disponível em breve.</p>
+
+</div>
+`;
